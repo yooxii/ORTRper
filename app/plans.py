f40a5c3189e7465fdadee4043337b9576e64748e
Lucas Li <2385014552@qq.com>
2025-02-18 01:56:48 +0800

    fix(ui): 修正Claims页面表单处理和删除功能

--- app/plans.py
+++ app/plans.py
@@ -19,12 +19,24 @@ def schedule():
 @bp.route('/Claims', methods=('GET', 'POST'))
 def Claims():
     db = get_plans_db()
-    claims = db.execute('SELECT * FROM claims').fetchall()
+    db_claims = db.execute('SELECT * FROM claims').fetchall()
+    
     if request.method == 'POST':
-        claim = dict(request.form)
-        db.execute('INSERT INTO Claims (claim_date, claim_no, PartNo, claim_qty, TestItem, SN, DC, REV, Work_Order, Remarks) VALUES (?,?,?,?,?,?,?,?,?,?)', (claim['claim_date'], claim['claim_no'], claim['PartNo'], claim['claim_qty'], claim['TestItem'], claim['SN'], claim['DC'], claim['REV'], claim['Work_Order'], claim['Remarks']))
-        db.commit()
-        claims = db.execute('SELECT * FROM Claims').fetchall()
-        flash('Claim submitted successfully')
-        return redirect(url_for('plans.Claims'))        
-    return render_template('plans/Claims.html', claimsPeek=claims)
+        claims = request.form.to_dict(flat=False)  # 使用to_dict(flat=False)获取表单数据为字典列表
+
+        for claim in zip(*claims.values()):  # 使用zip(*claims.values())同时迭代多个列表
+            claim_no = claim[1]
+            db_claim = db.execute('SELECT * FROM Claims WHERE claim_no = ?', (claim_no,)).fetchone()
+            if db_claim:
+                flash('Claim with number {} already exists'.format(claim_no))
+            else:
+                try:
+                    db.execute('INSERT INTO Claims (claim_date, claim_no, PartNo, claim_qty, TestItem, SN, DC, REV, Work_Order, Remarks) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', claim)
+                    db.commit()
+                    flash('Claim with number {} submitted successfully'.format(claim_no))
+                except Exception as e:
+                    flash('Error submitting claim with number {}: {}'.format(claim_no, str(e)))
+                    db.rollback()  # 回滚事务以防止数据不一致
+        return redirect(url_for('plans.Claims'))
+    
+    return render_template('plans/Claims.html', claimsPeek=db_claims)
