5b242d5f1a849bd91c48d78116bcc9439dd28cfb
Lucas Li <2385014552@qq.com>
2025-03-07 16:09:22 +0800

    feat: 初始化Django项目及基本应用结构

---
+++ ORTplans/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+
+class TSchedule(models.Model):
+    id = models.AutoField(primary_key=True)
+    JobNo = models.IntegerField()
+    QRT = models.BooleanField(default=False)
+    Product = models.CharField(max_length=25)
+    Customer = models.CharField(max_length=15)
+    PartNo = models.CharField(max_length=25)
+    Stage = models.CharField(max_length=5)
+    TestItem = models.CharField(max_length=50)
+    SampleSize = models.IntegerField()
+    TestPeriod = models.IntegerField()
+    Owner = models.CharField(max_length=50)
+    StartDate = models.DateField()
+    EndDate = models.DateField()
+    Status = models.CharField(max_length=10)
+    Upload_Elab = models.BooleanField(null=True)
+    Remark = models.TextField(null=True)
+
+class TCheckouts(models.Model):
+    id = models.AutoField(primary_key=True)
+    checkout_date = models.DateField()
+    checkout_no = models.CharField(max_length=20)
+    PartNo = models.CharField(max_length=15)
+    TestItem = models.CharField(max_length=50, null=True)
+    checkout_qty = models.DecimalField(max_digits=10, decimal_places=2)
+    SN = models.TextField()
+    DC = models.CharField(max_length=8)
+    REV = models.CharField(max_length=10)
+    Work_Order = models.CharField(max_length=40)
+    Remarks = models.TextField(null=True)
+    checkout_status = models.CharField(max_length=20, null=True)
