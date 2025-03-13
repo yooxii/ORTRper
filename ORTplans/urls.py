06e05e67274fd7f288593805199d80011ba0cd59
Lucas Li <2385014552@qq.com>
2025-03-13 17:01:43 +0800

    refactor: 修改领用信息模型字段类型及模板内容

--- ORTplans/urls.py
+++ ORTplans/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("checkouts", views.checkouts, name="checkouts"),
+    path("import_checkouts", views.import_checkouts, name="import_checkouts"),
+    path("export_checkouts", views.export_checkouts, name="export_checkouts"),
+    path("edit_checkouts", views.edit_checkouts, name="edit_checkouts"),
+    path("delete_checkouts", views.delete_checkouts, name="delete_checkouts"),
 ]
--- ORTplans/views.py
+++ ORTplans/views.py
@@ -3,6 +3,8 @@ from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
+from ORTplans.models import TCheckouts
+from rich import inspect
 
 
 def index(request):
@@ -11,4 +13,25 @@ def index(request):
 
 def checkouts(request):
     template = loader.get_template('ortplans/checkouts.html')
+    all_checkouts = TCheckouts.objects.all()
+    inspect(all_checkouts)
+    context = {
+        'all_checkouts': all_checkouts,
+    }
+    return HttpResponse(template.render(context, request=request))
+
+def import_checkouts(request):
+    template = loader.get_template('ortplans/import_checkouts.html')
+    return HttpResponse(template.render(request=request))
+
+def export_checkouts(request):
+    template = loader.get_template('ortplans/export_checkouts.html')
+    return HttpResponse(template.render(request=request))
+
+def edit_checkouts(request):
+    template = loader.get_template('ortplans/edit_checkouts.html')
+    return HttpResponse(template.render(request=request))
+
+def delete_checkouts(request):
+    template = loader.get_template('ortplans/delete.html')
     return HttpResponse(template.render(request=request))
