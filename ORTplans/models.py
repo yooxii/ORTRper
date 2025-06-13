84d2c1b07f03a1e0b355dafa4dfcd7a864395c61
Lucas Li <2385014552@qq.com>
2025-06-13 08:47:40 +0800

    feat(plugins): 添加了bootstrap图标库 refactor(ui): 优化了UI

--- ORTplans/models.py
+++ ORTplans/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class TSchedule(models.Model):
     id = models.AutoField(primary_key=True)
     JobNo = models.IntegerField()
@@ -19,10 +20,13 @@ class TSchedule(models.Model):
     Remark = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return (f"{self.JobNo}, {self.QRT}, {self.Product}, {self.Customer}, "
-                f"{self.PartNo}, {self.Stage}, {self.TestItem}, {self.SampleSize}, "
-                f"{self.TestPeriod}, {self.Owner}, {self.StartDate}, "
-                f"{self.EndDate}, {self.Status}, {self.Upload_Elab}, {self.Remark}")
+        return (
+            f"{self.JobNo}, {self.QRT}, {self.Product}, {self.Customer}, "
+            f"{self.PartNo}, {self.Stage}, {self.TestItem}, {self.SampleSize}, "
+            f"{self.TestPeriod}, {self.Owner}, {self.StartDate}, "
+            f"{self.EndDate}, {self.Status}, {self.Upload_Elab}, {self.Remark}"
+        )
+
 
 class TCheckouts(models.Model):
     id = models.AutoField(primary_key=True)
@@ -39,7 +43,8 @@ class TCheckouts(models.Model):
     checkout_status = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
-        return (f"{self.checkout_date}, {self.checkout_no}, {self.PartNo}, "
-                f"{self.TestItem}, {self.checkout_qty}, {self.SN}, {self.DC}, "
-                f"{self.REV}, {self.Work_Order}, {self.Remarks}, {self.checkout_status}")
-
+        return (
+            f"{self.checkout_date}, {self.checkout_no}, {self.PartNo}, "
+            f"{self.TestItem}, {self.checkout_qty}, {self.SN}, {self.DC}, "
+            f"{self.REV}, {self.Work_Order}, {self.Remarks}, {self.checkout_status}"
+        )
--- ORTplans/views.py
+++ ORTplans/views.py
@@ -1,42 +1,41 @@
 from django.shortcuts import render
-from django.template import loader
 
 # Create your views here.
-from django.http import HttpResponse
 from ORTplans.models import TCheckouts
 from rich import inspect
 
 
 def index(request):
-    template = loader.get_template("ortplans/index.html")
-    return HttpResponse(template.render(request=request))
+
+    return render(request=request, template_name="ortplans/index.html")
 
 
 def checkouts(request):
-    template = loader.get_template("ortplans/checkouts.html")
     all_checkouts = TCheckouts.objects.all()
     inspect(all_checkouts)
     context = {
         "all_checkouts": all_checkouts,
     }
-    return HttpResponse(template.render(context, request=request))
+    return render(
+        request=request, template_name="ortplans/checkouts.html", context=context
+    )
 
 
 def import_checkouts(request):
-    template = loader.get_template("ortplans/import_checkouts.html")
-    return HttpResponse(template.render(request=request))
+
+    return render(request=request, template_name="ortplans/import_checkouts.html")
 
 
 def export_checkouts(request):
-    template = loader.get_template("ortplans/export_checkouts.html")
-    return HttpResponse(template.render(request=request))
+
+    return render(request=request, template_name="ortplans/export_checkouts.html")
 
 
 def edit_checkouts(request):
-    template = loader.get_template("ortplans/edit_checkouts.html")
-    return HttpResponse(template.render(request=request))
+
+    return render(request=request, template_name="ortplans/edit_checkouts.html")
 
 
 def delete_checkouts(request):
-    template = loader.get_template("ortplans/delete.html")
-    return HttpResponse(template.render(request=request))
+
+    return render(request=request, template_name="ortplans/delete.html")
